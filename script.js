092d08de9d41bf599a1e43d5543bb72ead3d43ca
Marcus Powell <marcuspowell@MacBook-Pro-4.local>
2022-11-28 15:28:29 -0600

    finished assignment

--- script.js
+++ script.js
@@ -1,32 +1,40 @@
 // Write your JavaScript code here!
 
-//const { formSubmission } = require("./scriptHelper"); 
-//COMMENTING THE ABOVE LINE OUT FIXED A ERROR
 
 window.addEventListener("load", function() {
-    let pilotNameInput = document.querySelector("input[name=pilotName]")
-    let copilotNameInput = document.querySelector("input[name=copilotName]")
-    let fuelLevelInput = document.querySelector("input[name=fuelLevel]")
-    let cargoMassInput = document.querySelector("input[name=cargoMass]")
+    let pilotName = document.querySelector("input[name=pilotName]")
+    let copilotName = document.querySelector("input[name=copilotName]")
+    let fuelLevel = document.querySelector("input[name=fuelLevel]")
+    let cargoMass = document.querySelector("input[name=cargoMass]")
 
     let listedPlanets;
     // Set listedPlanetsResponse equal to the value returned by calling myFetch()
     let listedPlanetsResponse = myFetch();
+    console.log("listed planets response:",listedPlanetsResponse)
     listedPlanetsResponse.then(function (result) {
-        listedPlanets = result;
-        console.log(listedPlanets);
-    }).then(function () {
-        console.log(listedPlanets);
-        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-    })
+        console.log("result: ", result)
+        result.json().then(function (json) {
+            listedPlanets = json;
+            console.log("listedPlanets:",listedPlanets)
+            let planet = pickPlanet(listedPlanets);
+            addDestinationInfo(document, planet.name, planet.diameter, planet.star, planet.distance, planet.moons, planet.image);
+        })
+    });
+    // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+
+    
+
     const submitButton = document.getElementById("formSubmit");
-    submitButton.addEventListener("click", function() {
+    submitButton.addEventListener("click", function(event) {
         //Validate input
-        if (pilotNameInput.value === "" || copilotNameInput.value === "" || fuelLevelInput.value === "" || cargoMassInput.value === "") {
+        event.preventDefault();
+        if (validateInput(pilotName.value) === "Empty" || validateInput(copilotName.value) === "Empty" || validateInput(fuelLevel.value) === "Empty" || validateInput(cargoMass.value) === "Empty") {
             alert("All fields are required.");
-            preventDefault();
+        } else if (validateInput(fuelLevel.value) === "Not a Number" || validateInput(cargoMass.value) === "Not a Number") {
+            alert("Fuel level and cargo mass must be numbers.")
+        } else {
+            let list = ""
+            formSubmission(document, list, pilotName.value, copilotName.value, fuelLevel.value, cargoMass.value);
         }
-        let list = ""
-        formSubmission(document, list, pilotName.value, copilotName.value, fuelLevel.value, cargoMass.value);
     })
 });
--- scriptHelper.js
+++ scriptHelper.js
@@ -6,14 +6,25 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
    /*
                 <h2>Mission Destination</h2>
                 <ol>
-                    <li>Name: </li>
-                    <li>Diameter: </li>
+                    <li>Name: ${name}</li>
+                    <li>Diameter: ${diameter}</li>
                     <li>Star: ${star}</li>
-                    <li>Distance from Earth: </li>
-                    <li>Number of Moons: </li>
+                    <li>Distance from Earth: ${distance}</li>
+                    <li>Number of Moons: ${moons}</li>
                 </ol>
-                <img src="">
+                <img src="${imageUrl}">
    */
+  let missionTarget = document.getElementById("missionTarget");
+  missionTarget.innerHTML = `
+  <h2>Mission Destination</h2>
+  <ol>
+      <li>Name: ${name}</li>
+      <li>Diameter: ${diameter}</li>
+      <li>Star: ${star}</li>
+      <li>Distance from Earth: ${distance}</li>
+      <li>Number of Moons: ${moons}</li>
+  </ol>
+  <img src="${imageUrl}">`;
 }
 
 function validateInput(testInput) {
@@ -35,6 +46,7 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
    const cargoStatus = document.getElementById("cargoStatus");
    const launchStatus = document.getElementById("launchStatus");
 
+   
    pilotStatus.innerHTML = `Pilot name: ${pilot}`;
    copilotStatus.innerHTML = `Copilot name: ${copilot}`;
    
@@ -43,21 +55,34 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
     fuelStatus.innerHTML = "Not enough fuel for the journey";
     launchStatus.innerHTML = "Shuttle not ready for launch";
     launchStatus.style.color = "red";
-    preventDefault();
+   } else {
+    fuelStatus.innerHTML = "Fuel level high enough for launch"
+   }
+   if (cargoLevel > 10000) {
+    faultyItemsDiv.style.visibility = "visible";
+    cargoStatus.innerHTML = "Too much mass for the shuttle to take off"
+    launchStatus.innerHTML = "Shuttle not ready for launch"
+    launchStatus.style.color = "red";
+   } else {
+    cargoStatus.innerHTML = "Cargo mass low enough for launch";
+   }
+   if (fuelLevel >= 10000 && cargoLevel <= 10000) {
+    launchStatus.style.color = "green";
+    launchStatus.innerHTML = "Shuttle is ready for launch";
+    faultyItemsDiv.style.visibility = "visible";
    }
 }
 
 async function myFetch() {
     let planetsReturned;
 
-    planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then( function(response) {
-
-    });
+    planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json")
 
     return planetsReturned;
 }
 
 function pickPlanet(planets) {
+    return planets[Math.floor(Math.random()*planets.length)];
 }
 
 module.exports.addDestinationInfo = addDestinationInfo;
