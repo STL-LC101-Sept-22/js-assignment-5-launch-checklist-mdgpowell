c23035e40828e01853f4ad03733185b93f605127
Marcus Powell <marcuspowell@MacBook-Pro-4.local>
2022-12-05 16:46:43 -0600

    reverted change to list visibility

--- scriptHelper.js
+++ scriptHelper.js
@@ -68,7 +68,7 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
    if (fuelLevel >= 10000 && cargoLevel <= 10000) {
     launchStatus.style.color = "rgb(65, 159, 106)";
     launchStatus.innerHTML = "Shuttle is Ready for Launch";
-    list.style.visibility = "hidden";
+    list.style.visibility = "visible";
    }
 }
 
